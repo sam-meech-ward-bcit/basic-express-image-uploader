ebd627d1aee287c13d0628931e4419c0a55500c5
Sam Meech-Ward <sam@meech-ward.com>
2021-03-28 15:22:02 -0700

    connect to mongo every time

--- server.js
+++ server.js
@@ -16,25 +16,29 @@ const { MongoClient } = require('mongodb')
 const mongoUrl = process.env.MONGO_DB_URL || 'mongodb://localhost:27017'
 const dbName = process.env.MONGO_DB_NAME || 'image_upload_basic'
 const client = new MongoClient(mongoUrl, { useUnifiedTopology: true, useNewUrlParser: true })
-client.connect().then(() => {
 
+  
+app.get("/", async (req, res) => {
+  await client.connect()
   const db = client.db(dbName)
-
-  app.get("/", async (req, res) => {
-    const images = await db.collection("images").find().toArray()
-    res.render('index', { images })
-  })
-
-  app.post("/images", upload.single('image'), async (req, res) => {
-    const description = req.body.description
-    const filename = req.file.filename
-    await db.collection("images").insertOne({
-      description,
-      imageUrl: `/uploads/${filename}`
-    })
-    res.redirect("/")
-  })
-
-  const port = process.env.PORT || 8080
-  app.listen(port, () => console.log(`listening on port ${port}`))
+  const images = await db.collection("images").find().toArray()
+  res.render('index', { images })
+  await client.close()
 })
+
+app.post("/images", upload.single('image'), async (req, res) => {
+  await client.connect()
+  const db = client.db(dbName)
+  const description = req.body.description
+  const filename = req.file.filename
+  await db.collection("images").insertOne({
+    description,
+    imageUrl: `/uploads/${filename}`
+  })
+  res.redirect("/")
+  await client.close()
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`listening on port ${port}`))
+
