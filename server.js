5a1aad7de7f1ae92514cf0447309159b4239243c
Sam Meech-Ward <sam@meech-ward.com>
2021-04-01 10:32:47 -0700

    💩

--- server.js
+++ server.js
@@ -17,26 +17,42 @@ const mongoUrl = process.env.MONGO_DB_URL || 'mongodb://localhost:27017'
 const dbName = process.env.MONGO_DB_NAME || 'image_upload_basic'
 const client = new MongoClient(mongoUrl, { useUnifiedTopology: true, useNewUrlParser: true })
 
+async function connectClient() {
+  if (client.isConnect()) {
+    return
+  }
+  await client.connect()
+}
   
 app.get("/", async (req, res) => {
-  await client.connect()
-  const db = client.db(dbName)
-  const images = await db.collection("images").find().toArray()
-  res.render('index', { images })
-  await client.close()
+  try {
+    await connectClient()
+    const db = client.db(dbName)
+    const images = await db.collection("images").find().toArray()
+    res.render('index', { images })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({error: error.message})
+  }
 })
 
+
+
 app.post("/images", upload.single('image'), async (req, res) => {
-  await client.connect()
-  const db = client.db(dbName)
-  const description = req.body.description
-  const filename = req.file.filename
-  await db.collection("images").insertOne({
-    description,
-    imageUrl: `/uploads/${filename}`
-  })
-  res.redirect("/")
-  await client.close()
+  try {
+    await connectClient()
+    const db = client.db(dbName)
+    const description = req.body.description
+    const filename = req.file.filename
+    await db.collection("images").insertOne({
+      description,
+      imageUrl: `/uploads/${filename}`
+    })
+    res.redirect("/")
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error: error.message })
+  }
 })
 
 const port = process.env.PORT || 8080
